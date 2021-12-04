9a0fd933dfcb418a39f2bef04b0cd1c84db53bad
JaehyeonPaak <wogus392@gmail.com>
2021-12-05 00:37:32 +0900

    Create DogTest

--- src/Chapter7/Q4/DogTest.java
+++ src/Chapter7/Q4/DogTest.java
@@ -1,5 +1,15 @@
 package Chapter7.Q4;
 
 public class DogTest {
-    
+    public static void main(String[] args) {
+        Dog[] dogs = new Dog[5];
+        dogs[0] = new Dog("dog1", "type1");
+        dogs[1] = new Dog("dog2", "type1");
+        dogs[2] = new Dog("dog3", "type2");
+        dogs[3] = new Dog("dog4", "type3");
+        dogs[4] = new Dog("dog5", "type4");
+        for(Dog d: dogs) {
+            System.out.println(d.showDogInfo());;
+        }
+    }
 }
