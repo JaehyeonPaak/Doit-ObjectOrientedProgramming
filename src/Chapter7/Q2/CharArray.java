295b1ea7253c713221058a9e99cfed1df6181ee6
JaehyeonPaak <wogus392@gmail.com>
2021-12-05 00:01:01 +0900

    Transform A to a

---
+++ src/Chapter7/Q2/CharArray.java
@@ -0,0 +1,15 @@
+package Chapter7.Q2;
+
+public class CharArray {
+    public static void main(String[] args) {
+        char[] alphabets = new char[26];
+        char ch = 'A';
+        for(int i=0; i<alphabets.length; i++, ch++) {
+            alphabets[i] = ch;
+        }
+        for(char index: alphabets) {
+            int alp = (int)index + 32;
+            System.out.println((char)alp);
+        }
+    }
+}
