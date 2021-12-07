98a1043d56cf48dfd7b7fb86e4be2cb77a6aade3
JaehyeonPaak <wogus392@gmail.com>
2021-12-07 22:54:01 +0900

    Q5

---
+++ src/Chapter8/Q5/Genesis.java
@@ -0,0 +1,25 @@
+package Chapter8.Q5;
+
+public class Genesis extends Car {
+    
+    @Override
+    public void start() {
+        System.out.println("Genesis turn on the engine");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Genesis is speeding up");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Genesis is stopping");
+    }
+
+    @Override
+    public void turnoff() {
+        System.out.println("Genesis turn off the engine");
+    }
+    
+}
